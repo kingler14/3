51c7a4eeb452f5f3d1f35386b2761d39ce67d6f9
Владлен Дадашев <kingler1488@outlook.com>
2017-11-10 21:57:01 +1000

    commit5

--- 3/3/3.cpp
+++ 3/3/3.cpp
@@ -5,14 +5,13 @@ using namespace std;
 int main()
 {
 	string name;
-	cout << "Name: Vladlen, Rob" << endl;
+	cout << "Name: Vladlen, Nick" << endl;
 
 	cout << "Input name " << endl;
 
 	cin >> name;
 
 	if (name == "Vladlen") cout << "Hello Vladlen" << endl;
-	else if (name == "rob") cout << "Hello rob" << endl;
 	else if (name == "Nick") cout << "Hello Nick" << endl;
 	else cout << "Hello User" << endl;
 
